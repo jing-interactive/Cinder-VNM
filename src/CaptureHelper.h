d705176c3dfbe4157680ad8b9cd1c125f700fdbb
vinjn <vinjn.z@gmail.com>
2016-05-30 23:17:03 +0800

    Improve CaptureHelper.h

--- src/CaptureHelper.h
+++ src/CaptureHelper.h
@@ -17,25 +17,38 @@ struct CaptureHelper
         return texture != nullptr;
     }
     
-    void setup(int32_t width = 640, int32_t height = 480, const Capture::DeviceRef device = Capture::DeviceRef() )
+    bool isDirty()
     {
-        try {
-            capture = Capture::create( width, height, device );
+        bool ret = dirty;
+        dirty = false;
+        return ret;
+    }
+    
+    void setup(int32_t width = 640, int32_t height = 480, const Capture::DeviceRef device = Capture::DeviceRef())
+    {
+        try
+        {
+            capture = Capture::create(width, height, device);
             capture->start();
-            size = capture->getSize();
+            dirty = false;
             
             auto updateFn = [this]
             {
-                if ( capture && capture->checkNewFrame() )
+                if (capture && capture->checkNewFrame())
                 {
+                    dirty = true;
                     surface = *capture->getSurface();
-                    if ( ! texture ) {
+                    size = surface.getSize();
+                    
+                    if (!texture)
+                    {
                         // Capture images come back as top-down, and it's more efficient to keep them that way
                         gl::Texture::Format format;
 //                        format.loadTopDown();
                         texture = gl::Texture::create( surface, format);
                     }
-                    else {
+                    else
+                    {
                         texture->update( surface );
                     }
                 }
@@ -43,14 +56,16 @@ struct CaptureHelper
             App::get()->getSignalUpdate().connect(updateFn);
             
         }
-        catch ( ci::Exception &exc ) {
+        catch ( ci::Exception &exc )
+        {
             CI_LOG_EXCEPTION( "Failed to init capture ", exc );
         }
     }
     
     static void printDevices()
     {
-        for ( const auto &device : Capture::getDevices() ) {
+        for ( const auto &device : Capture::getDevices() )
+        {
             console() << "Device: " << device->getName() << " "
 #if defined( CINDER_COCOA_TOUCH )
             << ( device->isFrontFacing() ? "Front" : "Rear" ) << "-facing"
@@ -61,6 +76,7 @@ struct CaptureHelper
     
 private:
     
-    CaptureRef      capture;
+    CaptureRef  capture;
+    bool        dirty;
 
 };
