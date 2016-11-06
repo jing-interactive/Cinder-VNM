d1713d824f78476fabf687d424c52c259249ee92
vinjn <vinjn.z@gmail.com>
2016-11-06 13:53:09 +0800

    Update

--- include/GlslParamsBuilderImgui.h
+++ include/GlslParamsBuilderImgui.h
@@ -21,57 +21,18 @@ struct GlslParamsBuilderImgui
     
     GlslParamsBuilderImgui(ci::gl::GlslProgRef glsl)
     {
-        //        params->addSeparator();
-        //        auto label = glsl->getLabel();
-        //        params->addText(label);
         //
         glslProg = glsl;
         //
         // glsl reflection: uniform
         //
-        auto activeUniforms = glsl->getActiveUniforms();
+        activeUniforms = glsl->getActiveUniforms();
         sort(activeUniforms.begin(), activeUniforms.end(), [](const ci::gl::GlslProg::Uniform & a, const ci::gl::GlslProg::Uniform & b) -> bool{
             return a.getName() < b.getName();
         });
-        for (auto uniform : activeUniforms)
-        {
-            //            if (uniform.getCount() != 1) continue; // skip array
-            //            if (uniform.getUniformSemantic() != ci::gl::UNIFORM_USER_DEFINED) continue; // skip cinder-defined semantic
-            //            auto name = uniform.getName();
-            //            auto guiName = label + "::" + name;
-            //
-            //            switch (uniform.getType())
-            //            {
-            //                case GL_INT:
-            //                case GL_SAMPLER_2D:
-            //                case GL_SAMPLER_CUBE:
-            //                {
-            //                    addParam(glsl, params, guiName, name, namedInts);
-            //                    break;
-            //                }
-            //                case GL_FLOAT:
-            //                {
-            //                    addParam(glsl, params, guiName, name, namedFloats).step(0.1f);
-            //                    break;
-            //                }
-            //                case GL_FLOAT_VEC3:
-            //                {
-            //                    addParam(glsl, params, guiName, name, namedColors); // namedColors or namedVec3s?
-            //                    break;
-            //                }
-            //                case GL_BOOL:
-            //                {
-            //                    addParam(glsl, params, guiName, name, namedBools);
-            //                    break;
-            //                }
-            //                default:
-            //                {
-            //                    params->addText(guiName);
-            //                    break;
-            //                }
-            //            }
-        }
         
+        
+//        App::get()->getSignalUpdate().connect(std::bind(&GlslParamsBuilderImgui::drawImgui, this));
     }
     
     void applyUniforms()
@@ -85,6 +46,7 @@ struct GlslParamsBuilderImgui
     }
     
     ci::gl::GlslProgRef glslProg;
+    std::vector<gl::GlslProg::Uniform> activeUniforms;
     
     std::unordered_map<std::string, int> namedInts;
     std::unordered_map<std::string, bool> namedBools;
@@ -92,5 +54,53 @@ struct GlslParamsBuilderImgui
     std::unordered_map<std::string, ci::vec3> namedVec3s;
     std::unordered_map<std::string, ci::Color> namedColors;
     std::unordered_map<std::string, ci::ColorA> namedColorAs;
+    
+private:
+    void drawImgui()
+    {
+        auto label = glslProg->getLabel();
+        if (ui::CollapsingHeader(label.c_str(), ImGuiTreeNodeFlags_DefaultOpen))
+        {
+            for (auto uniform : activeUniforms)
+            {
+                //            if (uniform.getCount() != 1) continue; // skip array
+                //            if (uniform.getUniformSemantic() != ci::gl::UNIFORM_USER_DEFINED) continue; // skip cinder-defined semantic
+                //            auto name = uniform.getName();
+                //            auto guiName = label + "::" + name;
+                //
+                //            switch (uniform.getType())
+                //            {
+                //                case GL_INT:
+                //                case GL_SAMPLER_2D:
+                //                case GL_SAMPLER_CUBE:
+                //                {
+                //                    addParam(glsl, params, guiName, name, namedInts);
+                //                    break;
+                //                }
+                //                case GL_FLOAT:
+                //                {
+                //                    addParam(glsl, params, guiName, name, namedFloats).step(0.1f);
+                //                    break;
+                //                }
+                //                case GL_FLOAT_VEC3:
+                //                {
+                //                    addParam(glsl, params, guiName, name, namedColors); // namedColors or namedVec3s?
+                //                    break;
+                //                }
+                //                case GL_BOOL:
+                //                {
+                //                    addParam(glsl, params, guiName, name, namedBools);
+                //                    break;
+                //                }
+                //                default:
+                //                {
+                //                    params->addText(guiName);
+                //                    break;
+                //                }
+                //            }
+            }
+        }
+    };
+
 };
 
--- include/MiniConfigImgui.h
+++ include/MiniConfigImgui.h
@@ -69,19 +69,6 @@ namespace
         {
             takeScreenShot();
         }
-        
-#define GROUP_DEF(grp)                      } if (ui::CollapsingHeader(#grp)) {
-#define ITEM_DEF(type, var, default)        addImguiParam(#var, var);
-#define ITEM_DEF_MINMAX(type, var, default, Min, Max)  addImguiParam(#var, var, Min, Max);
-        if (true) {
-#include "item.def"
-        }
-#undef ITEM_DEF_MINMAX
-#undef ITEM_DEF
-#undef GROUP_DEF
-
-        ui::Separator();
-        
         if (ui::Button("Quit"))
         {
             App::get()->quit();
@@ -92,10 +79,22 @@ namespace
             isTestWindowOpened = !isTestWindowOpened;
         if (isTestWindowOpened)
             ui::ShowTestWindow(&isTestWindowOpened);
+        
+        
+#define GROUP_DEF(grp)                      } if (ui::CollapsingHeader(#grp, ImGuiTreeNodeFlags_DefaultOpen)) {
+#define ITEM_DEF(type, var, default)        addImguiParam(#var, var);
+#define ITEM_DEF_MINMAX(type, var, default, Min, Max)  addImguiParam(#var, var, Min, Max);
+        if (true) {
+#include "item.def"
+        }
+#undef ITEM_DEF_MINMAX
+#undef ITEM_DEF
+#undef GROUP_DEF
     }
 }
 
 void createConfigImgui()
 {
-    getWindow()->getSignalDraw().connect(drawImgui);
+    ui::initialize();
+    App::get()->getSignalUpdate().connect(drawImgui);
 }
