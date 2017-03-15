975a9cc4b74f7a2622c0b3f6ab9888bfdfe428a4
vinjn <vinjn.z@gmail.com>
2017-03-15 22:06:55 +0800

    Fix bug

--- templates/NonGame/src/_TBOX_PREFIX_App.cpp
+++ templates/NonGame/src/_TBOX_PREFIX_App.cpp
@@ -1,7 +1,6 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
-#include "cinder/CameraUi.h"
 
 #include "AssetManager.h"
 #include "MiniConfig.h"
@@ -17,15 +16,11 @@ class _TBOX_PREFIX_App : public App
     {
         createConfigUI({200, 200});
     
-        getWindow()->getSignalResize().connect([&] {
-        });
-
         getWindow()->getSignalKeyUp().connect([&](KeyEvent& event) {
             if (event.getCode() == KeyEvent::KEY_ESCAPE) quit();
         });
         
         getWindow()->getSignalDraw().connect([&] {
-            gl::setMatrices( mCam );
             gl::clear();
         });
     }
