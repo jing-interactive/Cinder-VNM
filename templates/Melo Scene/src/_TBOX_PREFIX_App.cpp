c7eb97650fb962c8b3e6205cd14912fb5c299f30
vinjn <vinjn.z@gmail.com>
2020-11-12 18:54:23 +0800

    Update "Melo Scene"

--- templates/Melo Scene/src/_TBOX_PREFIX_App.cpp
+++ templates/Melo Scene/src/_TBOX_PREFIX_App.cpp
@@ -7,6 +7,8 @@
 #include "AssetManager.h"
 #include "MiniConfigImgui.h"
 
+#include "melo.h"
+
 using namespace ci;
 using namespace ci::app;
 using namespace std;
@@ -17,8 +19,8 @@ struct _TBOX_PREFIX_App : public App
     {
         log::makeLogger<log::LoggerFileRotating>(fs::path(), "app.%Y.%m.%d.log");
 
-        auto aabb = am::triMesh(MESH_NAME)->calcBoundingBox();
-        mCam.lookAt(aabb.getMax() * 2.0f, aabb.getCenter());
+        mCam.setNearClip(1);
+        mCam.setFarClip(10000);
         mCamUi = CameraUi(&mCam, getWindow(), -1);
 
         createConfigImgui();
@@ -37,32 +39,31 @@ struct _TBOX_PREFIX_App : public App
                 quit();
         });
 
-        mGlslProg = am::glslProg(VS_NAME, FS_NAME);
-        mGlslProg->uniform("uTex0", 0);
-        mGlslProg->uniform("uTex1", 1);
-        mGlslProg->uniform("uTex2", 2);
-        mGlslProg->uniform("uTex3", 3);
+        {
+            mRoot = melo::createRootNode();
+            auto grid = melo::createGridNode();
+            mRoot->addChild(grid);
+
+            auto gltf = melo::createMeshNode(MESH_NAME);
+            mRoot->addChild(gltf);
+        }
 
         getSignalUpdate().connect([&] {
+            mRoot->treeUpdate();
         });
 
         getWindow()->getSignalDraw().connect([&] {
             gl::setMatrices(mCam);
             gl::clear();
 
-            gl::ScopedTextureBind tex0(am::texture2d(TEX0_NAME), 0);
-            gl::ScopedTextureBind tex1(am::texture2d(TEX1_NAME), 1);
-            gl::ScopedTextureBind tex2(am::texture2d(TEX2_NAME), 2);
-            gl::ScopedTextureBind tex3(am::texture2d(TEX3_NAME), 3);
-            gl::ScopedGlslProg glsl(mGlslProg);
-
-            gl::draw(am::vboMesh(MESH_NAME));
+            mRoot->treeDraw();
         });
     }
 
     CameraPersp mCam;
     CameraUi mCamUi;
     gl::GlslProgRef mGlslProg;
+    melo::NodeRef mRoot;
 };
 
 CINDER_APP(_TBOX_PREFIX_App, RendererGl, [](App::Settings *settings) {
